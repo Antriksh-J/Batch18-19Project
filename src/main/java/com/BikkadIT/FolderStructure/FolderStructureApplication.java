ac6684f8b4d6feb54a3a76bf84d276e57c81e53d
Antriksh-X <antrikshspacexgmail.com>
2025-02-07 13:34:30 +0530

    This is new project by Antriksh

---
+++ src/main/java/com/BikkadIT/FolderStructure/FolderStructureApplication.java
@@ -0,0 +1,17 @@
+package com.BikkadIT.FolderStructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderStructureApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application exection Started");
+		SpringApplication.run(FolderStructureApplication.class, args);
+		System.out.println("Application exection Ended");
+	}
+	
+	
+
+}
